6e3e55d8fde7ba5c4e4a1b1258bb3e57b38554d5
Prateek <prateek11077@iiitd.ac.in>
2014-02-15 08:15:42 +0530

    Views for home and index pages added.

--- CarPool/urls.py
+++ CarPool/urls.py
@@ -7,7 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CarPool.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'WebUI.views.home'),
+    url(r'^welcome/$', 'WebUI.views.welcome'),
+    url(r'^new_trip/$', 'WebUI.views.new_trip'),
+    url(r'^home/$', 'WebUI.views.home'),
     url(r'^save_journey/$', 'WebUI.views.save_journey'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- WebUI/views.py
+++ WebUI/views.py
@@ -3,9 +3,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from CarPool.settings import API_KEY
 from utils import clusterize_latlngs
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def welcome(request):
+    return render(request, 'index.html')
+
+def new_trip(request):
+    return render(request,'createJourney.html',API_KEY)
+
 def home(request):
-    return render(request, 'index.html', API_KEY)
+    return render(request, 'home.html')
 
 def save_journey(request):
     if request.is_ajax() and request.method == "POST":
@@ -15,3 +24,4 @@ def save_journey(request):
         distance = res['distance']
         clusters = clusterize_latlngs(cords, distance)
         return HttpResponse()
+
